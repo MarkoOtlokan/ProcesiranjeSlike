bc927afa2f465237919c65f69d2abf83b98dcceb
pantrasa <pantrasa@gmail.com>
2022-02-04 22:12:06 +0100

    improved contrast...some refactoring

--- ImgProc.py
+++ ImgProc.py
@@ -9,6 +9,13 @@ from scipy.interpolate import UnivariateSpline
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
 
 
+def create_lut(x, y, factor=0):
+    for ind in range(len(y)):
+        temp = y[ind] - x[ind]
+        y[ind] = x[ind] + factor * temp
+    spl = UnivariateSpline(x, y)
+    return spl(range(256))
+
 class PP:
     def __init__(self):
         self.img = None
@@ -48,29 +55,24 @@ class PP:
         return True
 
     def func2(self, p=0):
+        self.func4(round(p*(-1)*2.55))
         p = p / 100
+        p += 1
 
-        def help(x):
-            res = None
-            if x < 127.5 * cos(pi * (1 - p) / 4):
-                res = tan(pi * (1 - p) / 4) * x
-            elif 127.5 * cos(pi * (1 - p) / 4) <= x <= 255 - 127.5 * cos(pi * (1 - p) / 4):
-                res = ((1 - sin(pi * (1 - p) / 4)) / (1 - cos(pi * (1 - p) / 4))) * (
-                        x - 127.5 * cos(pi * (1 - p) / 4)) + 127.5 * sin(pi * (1 - p) / 4)
-            elif x > 255 - 127.5 * cos(pi * (1 - p) / 4):
-                res = tan(pi * (1 - p) / 4) * (x - 255 + 127.5 * cos(pi * (1 - p) / 4)) + 255 - 127.5 * sin(
-                    pi * (1 - p) / 4)
-            return int(res)
-        abc = [help(x) for x in range(256)]
-        logging.debug(f"lut table: {abc}\n")
-        contrast = np.array(abc, dtype=np.uint8)
-        # contrast = np.array([ (i-74)*p+74 for i in range (0,256)]).clip(0,255).astype('uint8')
-        # mada ovo nije moj kod...
-        # p (float): How much to adjust the contrast. Can be any
-        #             non negative number. 0 gives a solid gray image, 1 gives the
-        #             original image while 2 increases the contrast by a factor of 2.
-        lut = contrast #np.dstack((contrast, contrast, contrast))
-        self.img = cv2.LUT(self.orig_img, lut)
+        def help(y):
+            if p > 1:
+                abcd = None
+                if y < 128:
+                    abcd = (y-128*p)*p+128
+                else:
+                    abcd = (y-128)*p+p*128
+                return abcd
+            return (y-128)*p+128
+
+        contrast = np.array([help(i) for i in range(0, 256)]).clip(0, 255).astype('uint8')
+        logging.debug(f"lut table: {contrast}\n")
+        lut = contrast
+        self.img = cv2.LUT(self.img, lut)
         return True
 
     def func3(self, angle=0, scale=0):
@@ -118,40 +120,30 @@ class PP:
         self.img = rotate(self.orig_img, angle, scale=scale)
         return True
 
-    def func4(self, sat):
-        hsv = cv2.cvtColor(self.orig_img, cv2.COLOR_BGR2HSV)
+    def saturation_helper(self, org_img, sat):
+        hsv = cv2.cvtColor(org_img, cv2.COLOR_BGR2HSV)
         h, s, v = cv2.split(hsv)
         s = cv2.add(s, sat)
         s[s > 255] = 255
         s[s < 0] = 0
         final_hsv = cv2.merge((h, s, v))
         self.img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+
+    def func4(self, sat):
+        self.saturation_helper(self.orig_img, sat)
         return True
 
     def func5(self, warm = 0):
-        def create_LUT_8UC1(x, y, factor = 0):
-            for ind in range(len(y)):
-                temp = y[ind] - x[ind]
-                y[ind] = x[ind] + factor * temp
-            spl = UnivariateSpline(x, y)
-            return spl(range(256))
-
         warm = warm / 10
-        incr = create_LUT_8UC1([0, 50, 100, 150, 200, 245, 256], [0, 58, 124, 190, 229, 247, 256], warm)
-        decr = create_LUT_8UC1([0, 50, 100, 150, 200, 245, 256], [0, 41,  80, 123, 184, 212, 246], warm)
+        incr = create_lut([0, 50, 100, 150, 200, 245, 256], [0, 58, 124, 190, 229, 247, 256], warm)
+        decr = create_lut([0, 50, 100, 150, 200, 245, 256], [0, 41,  80, 123, 184, 212, 246], warm)
 
         identity = np.arange(256, dtype=np.dtype('uint8'))
         lut = np.dstack((decr, identity, incr))
         self.img = cv2.LUT(self.orig_img, lut).astype(np.uint8)
 
         sat_const = warm * 50
-        hsv = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV)
-        h, s, v = cv2.split(hsv)
-        s = cv2.add(s, sat_const)
-        s[s > 255] = 255
-        s[s < 0] = 0
-        final_hsv = cv2.merge((h, s, v))
-        self.img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+        self.saturation_helper(self.img, sat_const)
         return True
 
     def func6(self, factor = 0, gray = 0):
